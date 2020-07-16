2490ee4439afd113bec6042720a95243b9f982ed
sey cole <seyicole>
2020-07-16 22:36:33 +0100

    full design implementation almost done

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
+	$email = $_POST['email'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','contact_list');
+	if($conn->connect_error){
+		die('Connection Failed : ' .$conn->connect_error);
+	}else{
+		$stmt = $conn->prepare("insert into email(email)
+		values(?)");
+		$stmt->bind_param("s", $email);
+		$stmt->execute();
+		echo "registration succesful...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
