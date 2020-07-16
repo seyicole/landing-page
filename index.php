196101698c2eb4984163d7090d3c57a2bb6908c7
sey cole <seyicole>
2020-07-16 23:22:21 +0100

    added port number to index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	$email = $_POST['email'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','contact_list');
+	$conn = new mysqli('localhost:127.0.0.1','root','','contact_list');
 	if($conn->connect_error){
 		die('Connection Failed : ' .$conn->connect_error);
 	}else{
