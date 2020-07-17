0888273be1b2d07ee7e7e6484d0aa6bc05ee218b
sey cole <seyicole>
2020-07-17 10:56:31 +0100

    datbase adjusments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-function myFunction() {
-  alert("Subscribed succesfully")
-}
+//  let inputElt1 = document.getElementById('email1');
+// let inputElt = document.getElementById('email');
+// let btn = document.getElementById('button');
+//  let btn1 = document.getElementById('button1');
+
+// inputElt1.addEventListener("input", function(){
+//   btn1.disabled = (this.value === '');
+// })
+
+// inputElt.addEventListener("input", function(){
+//   btn.disabled = (this.value === '');
+// })
+
+// function myFunction() {
+//   alert("Subscribed succesfully")
+// }
