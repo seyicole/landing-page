71504ed2f09ae30590fa02c5e6653503dab4c6dc
sey cole <seyicole>
2020-07-17 11:37:09 +0100

    datbase adjusments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-//  let inputElt1 = document.getElementById('email1');
-// let inputElt = document.getElementById('email');
-// let btn = document.getElementById('button');
-//  let btn1 = document.getElementById('button1');
+ let inputElt1 = document.getElementById('email1');
+let inputElt = document.getElementById('email');
+let btn = document.getElementById('button');
+ let btn1 = document.getElementById('button1');
 
-// inputElt1.addEventListener("input", function(){
-//   btn1.disabled = (this.value === '');
-// })
+inputElt1.addEventListener("input", function(){
+  btn1.disabled = (this.value === '');
+})
 
-// inputElt.addEventListener("input", function(){
-//   btn.disabled = (this.value === '');
-// })
+inputElt.addEventListener("input", function(){
+  btn.disabled = (this.value === '');
+})
 
-// function myFunction() {
-//   alert("Subscribed succesfully")
-// }
+function myFunction() {
+  alert("Subscribed succesfully")
+}
